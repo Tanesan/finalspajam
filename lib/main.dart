47179c0fa6854a817fd7a14c0eeaf549b86c929b
Kazuki Toyoda <tennis0810kauppi@gmail.com>
2021-11-21 04:04:34 +0900

    added reception of argument at search.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,79 +19,47 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.from(
-          colorScheme:ColorScheme.light(primary: Color(0xffFC7D3C), background: Color(0xfff2f2f7)),
+          colorScheme: ColorScheme.light(
+              primary: Color(0xffFC7D3C), background: Color(0xfff2f2f7)),
           textTheme: TextTheme(
             headline1: GoogleFonts.roboto(
-                fontSize: 40,
-                fontWeight: FontWeight.w300,
-                letterSpacing: -1.5
-            ),
+                fontSize: 40, fontWeight: FontWeight.w300, letterSpacing: -1.5),
             headline2: GoogleFonts.roboto(
-                fontSize: 60,
-                fontWeight: FontWeight.w300,
-                letterSpacing: -0.5
-            ),
+                fontSize: 60, fontWeight: FontWeight.w300, letterSpacing: -0.5),
             headline3: GoogleFonts.roboto(
-                fontSize: 42,
-                color: Colors.black,
-                fontWeight: FontWeight.w600
-            ),
+                fontSize: 42, color: Colors.black, fontWeight: FontWeight.w600),
             headline4: GoogleFonts.roboto(
                 fontSize: 28,
                 color: Colors.black,
                 fontWeight: FontWeight.w800,
-                letterSpacing: 0.25
-            ),
-            headline5: GoogleFonts.roboto(
-                fontSize: 24,
-                fontWeight: FontWeight.w600
-            ),
+                letterSpacing: 0.25),
+            headline5:
+                GoogleFonts.roboto(fontSize: 24, fontWeight: FontWeight.w600),
             headline6: GoogleFonts.roboto(
-                fontSize: 22,
-                fontWeight: FontWeight.w800,
-                letterSpacing: 0.15
-            ),
+                fontSize: 22, fontWeight: FontWeight.w800, letterSpacing: 0.15),
             subtitle1: GoogleFonts.roboto(
-                fontSize: 18,
-                fontWeight: FontWeight.w800,
-                letterSpacing: 0.15
-            ),
+                fontSize: 18, fontWeight: FontWeight.w800, letterSpacing: 0.15),
             subtitle2: GoogleFonts.roboto(
-                fontSize: 14,
-                fontWeight: FontWeight.w500,
-                letterSpacing: 0.1
-            ),
+                fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 0.1),
             bodyText1: GoogleFonts.roboto(
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
-                letterSpacing: 0.5
-            ),
+                fontSize: 16, fontWeight: FontWeight.w600, letterSpacing: 0.5),
             bodyText2: GoogleFonts.roboto(
-                fontSize: 14,
-                fontWeight: FontWeight.w400,
-                letterSpacing: 0.25
-            ),
+                fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
             button: GoogleFonts.roboto(
-                fontSize: 14,
-                fontWeight: FontWeight.w500,
-                letterSpacing: 1.25
-            ),
+                fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.25),
             caption: GoogleFonts.roboto(
                 fontSize: 19,
                 color: Color(0xffD26919),
                 fontWeight: FontWeight.w600,
-                letterSpacing: 0.4
-            ),
+                letterSpacing: 0.4),
             overline: GoogleFonts.roboto(
-                fontSize: 10,
-                fontWeight: FontWeight.w400,
-                letterSpacing: 1.5
-            ),
+                fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
           )),
-//      home: Toppage(),
-      home: Timeline(),
+      home: Toppage(),
+//      home: Search(),
+//      home: Timeline(),
       routes: <String, WidgetBuilder>{
-         '/timeline': (BuildContext context) => Timeline(),
+        '/timeline': (BuildContext context) => Timeline(),
         '/search': (BuildContext context) => Search(),
       },
     );
--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:grouped_list/grouped_list.dart';
+import 'package:finalspajam/models/Argument.dart';
 
 class Search extends StatelessWidget {
   const Search({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container(
+    final Argument args =
+        ModalRoute.of(context)!.settings.arguments as Argument;
+
+    return Scaffold(
+        body: Container(
       decoration: BoxDecoration(
           gradient: RadialGradient(
         radius: 2,
@@ -22,49 +27,51 @@ class Search extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 64.0),
-                  child: Text("誰のTLを表示しますか?",
-                        style: Theme.of(context).textTheme.headline5),
-                ),
               Padding(
-                padding: EdgeInsets.only(left: 16, top: 32, right: 16, bottom: 32),
+                padding: EdgeInsets.only(top: 64.0),
+                child: Text("誰のTLを表示しますか?",
+                    style: Theme.of(context).textTheme.headline5),
+              ),
+              Padding(
+                padding:
+                    EdgeInsets.only(left: 16, top: 32, right: 16, bottom: 32),
                 child: Container(
                   height: 50,
                   decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.all(
-                      Radius.circular(32.0),
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        // color: Color(0xff8B98A4),
-                        spreadRadius: 3,
-                        color: Colors.black12,
-                        blurRadius: 10,
-                        offset: Offset(2, 2),
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(32.0),
                       ),
-                    ]),
+                      boxShadow: [
+                        BoxShadow(
+                          // color: Color(0xff8B98A4),
+                          spreadRadius: 3,
+                          color: Colors.black12,
+                          blurRadius: 10,
+                          offset: Offset(2, 2),
+                        ),
+                      ]),
                   child: TextField(
                     enabled: true,
                     // 入力数
                     // maxLength: 10,
                     // maxLengthEnforced: false,
-                    style: TextStyle(color: Color(0xff8B98A4), height: 1.4,fontSize: 18.0),
+                    style: TextStyle(
+                        color: Color(0xff8B98A4), height: 1.4, fontSize: 18.0),
                     obscureText: false,
                     maxLines: 1,
                     decoration: new InputDecoration(
                       prefixIcon: Icon(Icons.search),
-                          border: new OutlineInputBorder(
-                            borderRadius: const BorderRadius.all(
-                              const Radius.circular(32.0),
-                            ),
-                            borderSide: BorderSide(
-                              width: 0,
-                              style: BorderStyle.none,
-                            ),
-                          ),
-                          filled: true,
-                          fillColor: Colors.white,
+                      border: new OutlineInputBorder(
+                        borderRadius: const BorderRadius.all(
+                          const Radius.circular(32.0),
+                        ),
+                        borderSide: BorderSide(
+                          width: 0,
+                          style: BorderStyle.none,
+                        ),
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
                       // border: OutlineInputBorder(),
                       hintText: 'フォロー中のユーザーを検索',
                     ),
@@ -73,33 +80,33 @@ class Search extends StatelessWidget {
                   ),
                 ),
               ),
-
-    Expanded(
-    child: SingleChildScrollView(
-    child: GestureDetector(
-        onTap: () => print("As"),
-        child: Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(24.0),
-      ),
-                        elevation: 8.0,
-                        margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-                        child: Container(
-                          child: ListTile(
-                            contentPadding:
-                            EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-                            leading: CircleAvatar(
-                                backgroundImage: NetworkImage("https://tanemura.dev/img/icons.png")),
-                            title: Text("Keito Tanemura"),
-                            subtitle: Text("twitter discription"),
-                            trailing: Icon(Icons.arrow_forward),
-                          ),
-                        ),
-                      )))
-                  ),
+              Expanded(
+                  child: SingleChildScrollView(
+                      child: GestureDetector(
+                          onTap: () => print("As"),
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(24.0),
+                            ),
+                            elevation: 8.0,
+                            margin: EdgeInsets.symmetric(
+                                horizontal: 10.0, vertical: 6.0),
+                            child: Container(
+                              child: ListTile(
+                                contentPadding: EdgeInsets.symmetric(
+                                    horizontal: 20.0, vertical: 10.0),
+                                leading: CircleAvatar(
+                                    backgroundImage: NetworkImage(
+                                        "https://tanemura.dev/img/icons.png")),
+                                title: Text("Keito Tanemura"),
+                                subtitle: Text("twitter discription"),
+                                trailing: Icon(Icons.arrow_forward),
+                              ),
+                            ),
+                          )))),
             ],
           ),
-          ),
+        ),
       ),
     ));
   }
