537ab3628c0b6b8e43e223024252e922c3230eee
Kazuki Toyoda <tennis0810kauppi@gmail.com>
2021-11-21 12:46:07 +0900

    changed endpoint again

---
+++ lib/constants.dart
@@ -0,0 +1,2 @@
+//const String endpoint = "192.168.43.95:8080/";
+const String endpoint = "192.168.43.109:8080/";
--- lib/functions/getFollowees.dart
+++ lib/functions/getFollowees.dart
@@ -4,12 +4,14 @@ import 'dart:convert'; //httpレスポンスをJSON形式に変換用
 import 'package:finalspajam/models/FolloweeRequest.dart';
 import 'package:finalspajam/models/FolloweeResponse.dart';
 import 'package:finalspajam/data/followees.dart';
+import 'package:finalspajam/constants.dart';
 
 Future<FolloweeResponse> getFollowees(
     String accessToken, String secretAccessToken, String userId) async {
   //Future xxx async{} という記法
 //  return FolloweeResponse.fromJson(followees);
-  var url = "http://192.168.43.95:8080/follow_list";
+//  var url = "http://192.168.43.95:8080/follow_list";
+  var url = "http://${endpoint}follow_list";
 //  var url = "https://jphacks-server-3gabclop4q-dt.a.run.app/follow_list";
   var request = FolloweeRequest(
       accessToken: accessToken,
